d1f3e570076d46d804132e0feec48aaf6f54ffa8
Shotaro Nakamura <syoutarou1103@outlook.jp>
2026-03-18 12:37:04 +0900

    Switch GA to delayed main-thread loading and remove Partytown

--- src/components/partytown/partytown.tsx
+++
@@ -1,8 +0,0 @@
-import type { PartytownConfig } from '@qwik.dev/partytown/integration'
-import { partytownSnippet } from '@qwik.dev/partytown/integration'
-
-export interface PartytownProps extends PartytownConfig {}
-
-export const QwikPartytown = (props: PartytownProps) => {
-  return <script dangerouslySetInnerHTML={partytownSnippet(props)} />
-}
